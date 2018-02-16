2bc9094188ec3e3ac3577f93445baf37405b89e6
Nick Schmidt <nick@schmidtyanimation.com>
2018-02-16 17:42:10 -0600

    some book exercises

--- book_exercises_1.py
+++ book_exercises_1.py
@@ -14,13 +14,39 @@
 # base_rect = 6
 # print(base_rect * height_rect)
 
-name = "Nick"
-first = name
-age = 1000
-print(id(age))
-age = age + 1
-print(id(age))
-names = []
-print(id(names))
-names.append("Churro")
-print(id(names))
+# name = "Nick"
+# first = name
+# age = 1000
+# print(id(age))
+# age = age + 1
+# print(id(age))
+# names = []
+# print(id(names))
+# names.append("Churro")
+# print(id(names))
+
+# sleep_timers = [6.2, 7, 8, 5, 6.5, 7.1, 8.5]
+
+# print((sum(sleep_timers)) / len(sleep_timers))
+# print(297 % 3)
+# print(2 ** 10)
+
+leap_years = []
+cent_years = []
+year_list = [1800, 1900, 1903, 2000, 2002]
+for i in year_list:
+    if i % 4 == 0 and i % 100 != 0:
+        leap_years.append(i)
+    elif i % 4 == 0 and i % 100 == 0:
+        cent_years.append(i)
+
+for j in cent_years:
+    if j % 400 == 0:
+        leap_years.append(j)
+
+print(cent_years)
+print(leap_years)
+
+
+
+
