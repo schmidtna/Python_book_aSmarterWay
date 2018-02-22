e1b520f78b584572941c730c5b5bfac2b513dcf4
Nick Schmidt <nick@schmidtyanimation.com>
2018-02-22 08:18:43 -0600

    book exercises

--- book_exercises_1.py
+++ book_exercises_1.py
@@ -31,22 +31,40 @@
 # print(297 % 3)
 # print(2 ** 10)
 
-leap_years = []
-cent_years = []
-year_list = [1800, 1900, 1903, 2000, 2002]
-for i in year_list:
-    if i % 4 == 0 and i % 100 != 0:
-        leap_years.append(i)
-    elif i % 4 == 0 and i % 100 == 0:
-        cent_years.append(i)
+# leap_years = []
+# cent_years = []
+# year_list = [1800, 1900, 1903, 2000, 2002]
+# for i in year_list:
+#     if i % 4 == 0 and i % 100 != 0:
+#         leap_years.append(i)
+#     elif i % 4 == 0 and i % 100 == 0:
+#         cent_years.append(i)
 
-for j in cent_years:
-    if j % 400 == 0:
-        leap_years.append(j)
+# for j in cent_years:
+#     if j % 400 == 0:
+#         leap_years.append(j)
 
-print(cent_years)
-print(leap_years)
+# print(cent_years)
+# print(leap_years)
 
+name = "Nick"
+age = 35
+# print("My name is {} and I am {} years old".format(name, age))
+
+# paragraph = "\"Python is a great language!\", said Fred. \"I don't ever remember \n having this much fun before.\""
+# print(paragraph)
+# print("Omega one way " + "\u03A9" + " and another way " + "\N{GREEK CAPITAL LETTER OMEGA}")
+
+# item = "car"
+# cost = 13499.99
+# print("{:<10}${:>10,.2f}".format(item, cost))
+
+
+# print(help(name.find))
+# print(help(name.title))
+print(help(age.numerator))
+
+    
 
 
 
