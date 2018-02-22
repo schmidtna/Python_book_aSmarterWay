92e35bb666f2d7d906ad87802e332274fbaac33d
Nick Schmidt <nick@schmidtyanimation.com>
2018-02-22 11:30:16 -0600

    more book exercises

---
+++ book_exercises_2.py
@@ -0,0 +1,41 @@
+# age = 35
+# old = None
+# if age > 18:
+#     old = age
+# else:
+#     old = 0
+
+# print(bool(old))
+
+# name = "Nick"
+# name = name.lower()
+# second_half = "nopqrstuvwxyz"
+# for i in second_half:
+#     if name.startswith(i):
+#         print("got it")
+#         break
+#     elif i not in name:
+#         print("think again")
+
+# OR
+
+# name = "Nick"
+# name = name.lower()
+# alpha_check = name[0]
+# second_half = "nopqrstuvwxyz"
+
+# print(second_half.find(alpha_check))
+
+
+# class_names = ["me" , "you", "others"]
+# if class_names:
+#     print("The class has students.")
+# else:
+#     print("The class is empty.")
+
+# car = None
+
+# if car:
+#     print("You have a car!")
+# else:
+#     print("Get a taxi")
